77d9e2a0adb6ce2c8b3fb6baac05894736ec8283
lanxiaoke <350075999@qq.com>
2019-11-13 17:35:44 +0800

    ok

---
+++ HouseInvestTool/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace HouseInvestTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAll = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFlowIn = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLRL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAll
+            // 
+            this.txtAll.Location = new System.Drawing.Point(71, 30);
+            this.txtAll.Name = "txtAll";
+            this.txtAll.Size = new System.Drawing.Size(100, 21);
+            this.txtAll.TabIndex = 1;
+            this.txtAll.TextChanged += new System.EventHandler(this.txtAll_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "首付";
+            // 
+            // txtFlowIn
+            // 
+            this.txtFlowIn.Location = new System.Drawing.Point(71, 127);
+            this.txtFlowIn.Name = "txtFlowIn";
+            this.txtFlowIn.Size = new System.Drawing.Size(100, 21);
+            this.txtFlowIn.TabIndex = 4;
+            this.txtFlowIn.TextChanged += new System.EventHandler(this.txtFlowIn_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "月租";
+            // 
+            // txtLRL
+            // 
+            this.txtLRL.Location = new System.Drawing.Point(71, 228);
+            this.txtLRL.Name = "txtLRL";
+            this.txtLRL.ReadOnly = true;
+            this.txtLRL.Size = new System.Drawing.Size(55, 21);
+            this.txtLRL.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 12);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "% (大于5%为最低标准)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "利润率";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "贷款";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 161);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 19;
+            this.textBox2.Text = "0.049";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "年利率";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(71, 94);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "25";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "还款年数";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(71, 200);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(55, 21);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(15, 203);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 12);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "月均还款";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(173, 68);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(17, 12);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "万";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(173, 37);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(17, 12);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "万";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 293);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLRL);
+            this.Controls.Add(this.txtFlowIn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAll);
+            this.Controls.Add(this.label4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "房产投资计算工具(蓝建荣)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAll;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFlowIn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLRL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+
+    }
+}
+
---
+++ HouseInvestTool/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HouseInvestTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString().Equals("r", StringComparison.OrdinalIgnoreCase))
+            {
+                txtAll.Focus();
+                txtAll.SelectAll();
+            }
+        }
+
+        private void txtAll_TextChanged(object sender, EventArgs e)
+        {
+            JS();
+        }
+
+        private void txtFlowOut_TextChanged(object sender, EventArgs e)
+        {
+            JS();
+        }
+
+        private void txtFlowIn_TextChanged(object sender, EventArgs e)
+        {
+            JS();
+        }
+
+        private void JS1()
+        {
+            double all;
+            double.TryParse(txtAll.Text.Trim(), out all);
+            if (all == 0)
+            {
+                txtLRL.Text = "0";
+                return;
+            }
+            all = all * 10000;
+
+            double flowOut;
+            double.TryParse(textBox4.Text.Trim(), out flowOut);
+
+            double flowIn;
+            double.TryParse(txtFlowIn.Text.Trim(), out flowIn);
+
+            txtLRL.Text = ((flowIn - flowOut) * 1200.00 / all).ToString("0.##");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            JS();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            JS();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            JS();
+        }
+
+        //等额本息计算公式： ［贷款本金 × 月利率 × （ 1 ＋月利率）＾还款月数］ ÷ ［（ 1 ＋月利率）＾还款月数－ 1 ］  
+        private void JS()
+        {
+            JS2();
+            JS1();
+            
+        }
+
+        private void JS2()
+        {
+            double all;
+            double.TryParse(textBox1.Text.Trim(), out all);
+            if (all == 0)
+            {
+                textBox4.Text = "0";
+                return;
+            }
+            all = all * 10000;
+
+            double lv;
+            double.TryParse(textBox2.Text.Trim(), out lv);
+            lv = lv / 12;
+
+            int months;
+            int.TryParse(textBox3.Text.Trim(), out months);
+
+            months = months * 12;
+
+            var total = (all * lv * Math.Pow(1 + lv, months)) / ((Math.Pow(1 + lv, months)) - 1);
+
+            textBox4.Text = total.ToString("0.##");
+        }
+    }
+}
